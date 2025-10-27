210e56f528e9dace448a22f6c7fbcbedb504dfb9
ayush <ayush.sharma@clouddrove.com>
2025-10-27 18:51:34 +0530

    api permisssions updated dynamically

--- main.tf
+++ main.tf
@@ -13,29 +13,17 @@ resource "azuread_application" "sp" {
     logout_url = length(var.front_channel_logout_urls) > 0 ? var.front_channel_logout_urls[0] : null
   }
 
-  dynamic "required_resource_access" {
-    for_each = var.enable_api_permission ? [1] : []
+dynamic "required_resource_access" {
+  for_each = var.enable_api_permission ? [1] : []
     content {
       resource_app_id = "00000003-0000-0000-c000-000000000000"
 
-      resource_access {
-        id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" # User.Read
-        type = "Scope"
-      }
-
-      resource_access {
-        id   = "64a6cdd6-aab1-4aaf-94b8-3cc8405e90d0" # Email
-        type = "Scope"
-      }
-
-      resource_access {
-        id   = "14dad69e-099b-42c9-810b-d002981feec1" # Profile
-        type = "Scope"
-      }
-
-      resource_access {
-        id   = "37f7f235-527c-4136-accd-4a02d197296e" # OpenID
-        type = "Scope"
+      dynamic "resource_access" {
+        for_each = var.api_permissions
+        content {
+          id   = resource_access.value.id
+          type = resource_access.value.type
+        }
       }
     }
   }
@@ -61,7 +49,6 @@ resource "azuread_application_app_role" "admin" {
   value                = "Admin"
   role_id              = uuid()
 }
-
 
 # -------------------------------
 # Service Principal
